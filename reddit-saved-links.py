5009dadc6728c13bed21ae3c7b9f233f45a56976
TomParys <tom.parys@gmail.com>
2012-04-15 22:05:22 +0200

    First fully working version.
    Prints results to an HTML file.

--- reddit-saved-links.py
+++ reddit-saved-links.py
@@ -1,13 +1,51 @@
-import reddit
-r = reddit.Reddit(user_agent='my_cool_application')
-submissions = r.get_subreddit('opensource').get_hot(limit=5)
-[str(x) for x in submissions]
-
-"""
+# -*- coding: utf-8 -*-
+import re
+import time
 import reddit
 
-r = reddit.Reddit(user_agent='A very timid bot of u/GNeps')
-r.login('gneps','enigma')
+def _getSubreddit(submission):
+	"""Returns subreddit of said submission, or "Saved comments" if it's not link to a submission but a comment."""
+	try:
+		return re.findall("www.reddit.com/r/([^/]*)/", submission.permalink)[0]
+	except IndexError:
+		#raise Error("Cannot parse submission subreddit for: " + submission.permalink)
+		return "Saved comments"
 
-print r.get_subreddit('android').get_top(limit=10)
-"""
+def getSavedLinksBySubreddit(username, password):
+	r = reddit.Reddit(user_agent='A very timid bot of u/GNeps')
+	r.login(username, password)
+	submissions = r.get_saved_links(limit=None)
+
+	saved = dict()
+	for x in submissions:
+		saved.setdefault(_getSubreddit(x), []).append(x)
+	return saved
+
+def printSavedLinksBySubreddit():
+	saved = getSavedLinksBySubreddit('gneps','enigma')
+	for subreddit, submissions in saved.items():
+		print subreddit
+		for submission in submissions:
+			print "\t", str(submission)
+
+def htmlSavedLinksBySubreddit():
+	saved = getSavedLinksBySubreddit('gneps','enigma')
+	html = open("saved-links.html", "w")
+	
+	html.write("<h1>Saved links from Reddit sorted by subreddits</h1>\n")
+	html.write("I found a total of %s saved links in %s subreddits.\n" % (sum(len(b) for (a, b) in saved.items()), len(saved.items())))
+	
+	for subreddit, submissions in saved.items():
+		html.write("<h2>" + subreddit + "</h2>\n<ul>\n")
+		
+		for submission in submissions:
+			try:
+				html.write("\t<li><a href='%s'>%s</a>\n" % (submission.permalink, str(submission)))
+			except UnicodeEncodeError:
+				#print "UnicodeError for:", submission.permalink
+				html.write("\t<li><a href='%s'>%s</a>\n" % (submission.permalink, "Name unknown"))
+		html.write("</ul>\n\n\n")
+
+	
+if __name__ == "__main__":
+	htmlSavedLinksBySubreddit()
