bf0255afe850e97083cebef31a6c691572bdeb6c
TomParys <tom.parys@gmail.com>
2012-04-16 10:45:26 +0200

    Made handling file safer.
    Added with statement that automatically closes the file after use.

--- reddit-saved-links.py
+++ reddit-saved-links.py
@@ -31,25 +31,25 @@ def printSavedLinksBySubreddit():
 
 def htmlSavedLinksBySubreddit():
 	saved = getSavedLinksBySubreddit('gneps','enigma')
-	html = open("saved-links.html", "w")
 	
-	html.write("<html>\n<body>\n\n");
-	html.write("<h1>Saved links from Reddit sorted by subreddits</h1>\n")
-	html.write("I found a total of %s saved links in %s subreddits.\n\n\n" % (
-			sum(len(b) for (a, b) in saved), len(saved)))
-	
-	for subreddit, submissions in saved:
-		html.write("<h2>" + subreddit + "</h2>\n<ul>\n")
+	with open("saved-links.html", "w") as html:
+		html.write("<html>\n<body>\n\n");
+		html.write("<h1>Saved links from Reddit sorted by subreddits</h1>\n")
+		html.write("I found a total of %s saved links in %s subreddits.\n\n\n" % (
+				sum(len(b) for (a, b) in saved), len(saved)))
 		
-		for submission in submissions:
-			try:
-				html.write("<li><a href='%s'>%s</a>\n" % (submission.permalink, str(submission)))
-			except UnicodeEncodeError:
-				#print "UnicodeError for:", submission.permalink
-				html.write("<li><a href='%s'>%s</a>\n" % (submission.permalink, "Name unknown"))
-		html.write("</ul>\n\n\n")
-	
-	html.write("</body>\n</html>\n");
+		for subreddit, submissions in saved:
+			html.write("<h2>" + subreddit + "</h2>\n<ul>\n")
+			
+			for submission in submissions:
+				try:
+					html.write("<li><a href='%s'>%s</a>\n" % (submission.permalink, str(submission)))
+				except UnicodeEncodeError:
+					#print "UnicodeError for:", submission.permalink
+					html.write("<li><a href='%s'>%s</a>\n" % (submission.permalink, "Name unknown"))
+			html.write("</ul>\n\n\n")
+		
+		html.write("</body>\n</html>\n");
 
 	
 if __name__ == "__main__":
